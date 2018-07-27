bf1094dc29c390eb5f32450655e93d5e47a40cc3
Artanic <599253487@qq.com>
2018-07-27 17:12:53 +0800

    update vision2.0

--- Visiting a friend.py
+++ Visiting a friend.py
@@ -1,33 +1,33 @@
-a = int(input('输入传送点个数:'))
-b = int(input('输入朋友家位置:'))
+n = int(input('输入传送点个数:'))
+m = int(input('输入朋友家位置:'))
 i = 0
 c = 0
-n = 1
+d = 1
 list1 = []
 list2 = []
-while i < a:
+while i < n:
     c += 1
     h = int(input('传送门' + str(c) + '起始点:'))
     e = int(input('传送门' + str(c) + '结束点:'))
     i += 1
     list1.append(h)
     list2.append(e)
-# print('传送门起始位置：' + str(list1), '传送门结束位置：' + str(list2))
-if list2[-1] >= b:
-     #print(list2[-1], b)
+# else:print('传送门起始位置：' + str(list1), '传送门结束位置：' + str(list2))
+if list2[-1] >= m:
+   # print(list2[-1], m)
     if list1[0] == 0:
-       # print(list1[n-1])
-       # print(list2[n])
-       # print(n)
-        while n < a:
+      #  print(list1[d-1])
+     #   print(list2[d])
+     #   print(d)
+        while d < n:
             # 1,2个条件限制后一个传送门起始位置在前一个门范围内3,限制后传送门为前进
-            if list2[n-1] >= list1[n]  and list1[n-1] < list1[n] and list2[n-1] < list2[n]:
-                pass
-                n += 1
-            #print(n)
-            else:break
-        print('No')
-        if n == a:
-            print('Yes')
+            if list2[d-1] >= list1[d] and list2[d-1] >= list2[d]:
+               print('no')
+               exit(0)
+            elif list2[d-1] <= list1[d] and list2[d-1] <= list2[d]:
+                print('yes')
+     #           print(d)
+                d += 1
+            else:exit(0)
     else:print('No')
 else: print('No')
