e748061220b886520aeb8f455ade86fc65cff8ff
Artanic <599253487@qq.com>
2018-08-22 20:46:21 +0800

    update the last easy question

--- Boom.py
+++ Boom.py
@@ -1,7 +1,27 @@
+import itertools
+
 N = int(input())
 i = 1
-dict = {}
+k = 1
+s = 0
+boxlist = []
+zuhe = ()
+color = set()
 while i <= N:
-    n, m = map(int, input().split())
-    dict[n] = m
-print(dict)
+    n_i, m_i = map(int, input().split())
+    boxlist.append({m_i, n_i})
+    while k <= len(boxlist):
+        zuhe = itertools.combinations(boxlist, k)
+        for a in zuhe:
+            for b in a:
+                for c in b:
+                    color.add(c)
+            if len(color) == k:
+                print(i)
+                exit(0)
+            color = set()
+        k += 1
+    i += 1
+    k = 1
+print('no')
+
